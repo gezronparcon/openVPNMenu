cb9775b55b9162d47bb8ad73598a14911218cefe
kikay001 <gezronparcon@gmail.com>
2018-03-31 12:03:06 +0800

    Create jae.sh

---
+++ jae.sh
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+</head>
+<body>
+
+
+<br><center><font color='#B22222'><b>================================================<b></br></center></font>
+<br><center><font color='#B22222'><b>=========================== SERVER OWN BY JAE-AUGUST=======================</br></center></font>
+<br><center><font color='#191970'><b>================================================<b></br></center></font>
+<br><center><font size="100"	color='#008000'><b>=======================&#128128;=======================<b></br></center></font>
+<br><center><font size="100"	color='#008000'><b>=======================&#128128;=======================<b></br></center></font>
+<br><font color='#FF7373'>*************!!!WARNING!!!*************</br></font>
+<br></br>
+<br><font color='#FF7373'>NO SPAM !!!</br></font>
+<br><font color='#FF7373'>NO DDOS !!!</br></font>
+<br><font color='#FF7373'>NO HACKING !!!</br></font>
+<br><font color='#FF7373'>NO CARDING !!!</br></font>
+<br><font color='#FF7373'>NO CHILD-PORNOGRAPHY !!!</br></font>
+<br><font color='#FF7373'>NO MULTI-LOGIN !!!</br></font>
+<br><font color='#FF7373'>FOLLOW THE RULES OR</br></font>
+<br><font color='#FF7373'>YOUR ACCOUNT WILL BE BANNED</br></font><br></br>
+<br><font color='#FF7373'>=======================================</br></font>
+</body>
+</html>

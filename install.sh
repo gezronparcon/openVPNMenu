9fb5fdd74144007dda5e77f3bd64d6d9061af730
kikay001 <gezronparcon@gmail.com>
2018-03-31 14:13:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -179,7 +179,7 @@ rm -rf /root/ddos-deflate-master.zip
 
 # banner
 rm /etc/issue.net
-wget -O /etc/issue.net "https://raw.githubusercontent.com/gezronparcon/openVPNMenu/master/issue.net"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/gezronparcon/openVPNMenu/master/jae-issue.net"
 sed -i 's@#Banner@Banner@g' /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 service ssh restart
